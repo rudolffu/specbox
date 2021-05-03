a45367073c48250b5cf7653d955dc57475f83c7e
Yuming Fu <fuympku@outlook.com>
2021-05-03 11:50:49 +0800

    added necessary files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="specbox",
+    version="0.0.1",
+    author="Yuming Fu",
+    author_email="fuympku@outlook.com",
+    description=" ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rudolffu/specbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ specbox/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# __all__ = []
+# from . xxx import *
+# __all__ += xxx.__all__
