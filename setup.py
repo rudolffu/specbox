5aed9fe5441d86776ea59a85ff8c1086b9544082
rudolffu <fuympku@outlook.com>
2025-02-18 17:13:00 +0100

    bumping version to 0.1.0-beta

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="specbox",
-    version="0.1.0-alpha",
+    version="0.1.0-beta",
     author="Yuming Fu",
     author_email="fuympku@outlook.com",
     description="specbox - A simple tool to manipulate and visualize optical spectra for astronomical research.",
