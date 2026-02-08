3d0d755f296d118b21ed57b68dac3e99374bdc8b
rudolffu <fuympku@outlook.com>
2026-02-08 10:46:55 +0100

    Move packaging metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="specbox",
-    version="1.0.0-pre",
-    author="Yuming Fu",
-    author_email="fuympku@outlook.com",
-    description="specbox - A simple tool to manipulate and visualize optical spectra for astronomical research.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rudolffu/specbox",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "astropy",
-        "pyqtgraph",
-        "PySide6",
-        "specutils",
-        "matplotlib",
-        "pandas",
-        "requests",
-        "pillow",
-        "astroquery",
-    ],
-    package_data={
-        'specbox': ['data/*'],
-    },
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.9',
-)
+setup()
--- specbox/__init__.py
+++ specbox/__init__.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+from __future__ import annotations
+
+from importlib.metadata import PackageNotFoundError, version as dist_version
+
+try:
+    __version__ = dist_version("specbox")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
+
 __all__ = []
 from . basemodule import *
 __all__ += basemodule.__all__
